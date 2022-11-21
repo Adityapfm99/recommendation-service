ca25a18c1754a31145079151905b939fd12d50f5
adityanugraha23 <aditya@hungryhub.com>
2022-11-21 13:56:51 +0700

    added process env for store URI MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+require('dotenv').config()
 import { RecommendationController } from './controller/recommendation/recommendation.controller';
 import { RecommendationSchema } from './schema/recommendation.schema';
 import { RecommendationService } from './service/recommendation/recommendation.service';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017',{dbName: 'recommendation'}),
+  imports: [MongooseModule.forRoot(`${process.env.MONGO_DB_URI}`,{dbName: 'recommendation'}),
   MongooseModule.forFeature([{ name: 'Recommendation', schema: RecommendationSchema }])],
   controllers: [AppController,RecommendationController],
   providers: [AppService,RecommendationService],
